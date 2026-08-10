d2b66a633a8c77d209ee79b6b54e88ec25c448a6
wenrui23 <251331M@mymail.nyp.edu.sg>
2026-08-10 19:37:27 +0800

    Update techniques.js

--- techniques.js
+++ techniques.js
@@ -1,4 +1,7 @@
+// Allows a Bootstrap tab on the Guides page to be opened directly using a URL hash
 document.addEventListener("DOMContentLoaded", function() {
+
+      // Check the current URL hash
       function activateTabFromHash() {
         if (window.location.hash) {
           let hash = window.location.hash;
@@ -10,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
       }
 
+      // Activate the correct tab on initial page load
       activateTabFromHash();
       window.addEventListener("hashchange", activateTabFromHash);
     });
